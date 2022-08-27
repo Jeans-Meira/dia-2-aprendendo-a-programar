793ee62e104f7ea6ddf49e9e81598fa7fc70a6d8
Jean Meira <jeanslima65@gmail.com>
2022-08-27 11:41:03 -0300

    mensagem de commit

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,20 @@
+
+
+let numero = 16
+let n = 0
+
+console.log(1)
+console.log(3)
+console.log(5)
+console.log(7)
+console.log(9)
+console.log(11)
+console.log(13)
+console.log(15)
+
+let ehImpar = numero % 2 != 0
+if (ehImpar) {
+    console.log(`${numero} é ímpar!`) 
+} else {
+    console.log(`${numero} é par!`)
+}
